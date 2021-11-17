b0dbcc9cd64afcfea4beb89545e39544068ca3d2
kts of kettek <kts@kettek.net>
2021-11-17 03:27:26 -0800

    Separate defaults interface

--- src/interfaces/Module.ts
+++ src/interfaces/Module.ts
@@ -1,15 +1,17 @@
 import type { SvelteComponent } from "svelte"
 import type { BoxInterface } from "./Box"
 
+export interface ModuleDefaultsInterface {
+    title: string
+    box: BoxInterface
+    settings: any
+}
+
 export interface ModuleInterface {
     uuid: string
     title: string
     settingsComponent?: SvelteComponent
     instanceComponent?: SvelteComponent
     itemComponent?: SvelteComponent
-    defaults: {
-        title: string
-        box: BoxInterface
-        settings: any
-    }
+    defaults: ModuleDefaultsInterface
 }
