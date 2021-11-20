11e61e5d762348c58d1fe1d46c0e15889da6c9f7
kts of kettek <kts@kettek.net>
2021-11-20 03:41:47 -0800

    Adjust settings interface to be a proper key=>value store

--- src/interfaces/ModuleInstance.ts
+++ src/interfaces/ModuleInstance.ts
@@ -8,5 +8,5 @@ export interface ModuleInstanceInterface {
     uuid: string // used to identify this module
     box: BoxInterface // used to size module instance component
     moduleUUID: string // UUID Reference to the associated module
-    settings: any // Passed to module
+    settings: {[key: string]: any} // Passed to module
 }
