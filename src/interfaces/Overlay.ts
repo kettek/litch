34e2e2e568ea9173e92b76b8fe7d78ef705fe240
kts of kettek <kts@kettek.net>
2021-12-04 07:56:45 -0800

    Fix offensive whitespace

--- src/interfaces/Overlay.ts
+++ src/interfaces/Overlay.ts
@@ -4,11 +4,11 @@ import type { ModuleInstanceInterface } from './ModuleInstance'
  * An OverlayInterface is a collection of canvas sizing, active modules, their settings, and their dimensions within a canvas.
  */
 export interface OverlayInterface {
-    title: string
-    uuid: string
-    canvas: BoxInterface
-    modules: ModuleInstanceInterface[]
-    openAvailableModules: boolean
-    openActiveModules: boolean
-    openSettings: boolean
+	title: string
+	uuid: string
+	canvas: BoxInterface
+	modules: ModuleInstanceInterface[]
+	openAvailableModules: boolean
+	openActiveModules: boolean
+	openSettings: boolean
 }
