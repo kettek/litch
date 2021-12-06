15dc7c271dc22d43fd67fdab4e47868f6ca5aed6
kts of kettek <kts@kettek.net>
2021-12-06 05:34:41 -0800

    Thoroughly adjust AssetListing

--- src/AssetsListing.svelte
+++ src/AssetsListing.svelte
@@ -2,34 +2,102 @@
 	import type { Asset } from "./interfaces/Asset"
 	import { isAssetFiltered } from './assets'
 
-	export let selectedUUID: string
-	export let assets: Asset[] | undefined
-	export let filter: string
+	export let selector: boolean = false
+	export let multiple: boolean = false
+	export let focused: string = ''
+	export let selected: string[] = []
+	export let assets: Asset[] | undefined = []
+	export let filter: string = ''
+
+	function onAssetClick(e: MouseEvent, uuid: string) {
+		if (selected.includes(uuid)) return
+
+		if (multiple) {
+			if (e.shiftKey) {
+				let focusedIndex = selected.indexOf(focused)
+				let targetIndex = selected.indexOf(uuid)
+				// FIXME: lazy code duplication
+				if (targetIndex < focusedIndex) {
+					for (let i = focusedIndex; i > targetIndex; i--) {
+						let uuid = selected[i]
+						if (e.ctrlKey) {
+							if (selected.includes(uuid)) {
+								selected = selected.filter(v=>v===uuid)
+							} else {
+								selected = [ ...selected, uuid ]
+							}
+						} else {
+							if (!selected.includes(uuid)) {
+								selected = [ ...selected, uuid ]
+							}
+						}
+					}
+				} else if (targetIndex > focusedIndex) {
+					for (let i = focusedIndex; i < targetIndex; i++) {
+						let uuid = selected[i]
+						if (e.ctrlKey) {
+							if (selected.includes(uuid)) {
+								selected = selected.filter(v=>v===uuid)
+							} else {
+								selected = [ ...selected, uuid ]
+							}
+						} else {
+							if (!selected.includes(uuid)) {
+								selected = [ ...selected, uuid ]
+							}
+						}
+					}
+				}
+			} else if (e.ctrlKey) {
+				if (selected.includes(uuid)) {
+					selected = selected.filter(v=>v===uuid)
+				} else {
+					selected = [ ...selected, uuid ]
+				}
+			} else {
+				selected = [ uuid ]
+			}
+		} else {
+			selected = [ uuid ]
+		}
+
+		focused = uuid
+	}
 </script>
 
-{#if assets}
-	{#each assets.sort((a,b)=>a.name.localeCompare(b.name)) as asset (asset.uuid)}
-		{#if !isAssetFiltered(asset, filter)}
-			<div on:click={()=>selectedUUID = asset.uuid} title='{asset.uuid}' class='asset'>
-				<header>{asset.name}</header>
-				<article>
-					{#if asset.mimetype.startsWith('image')}
-						<img alt={asset.uuid} src={asset.redirectedSource||asset.originalSource}/>
-					{:else if asset.mimetype.startsWith('video')}
-						<video controls src={asset.redirectedSource||asset.originalSource}>
-							<track kind="captions" />
-						</video>
-					{:else if asset.mimetype.startsWith('audio')}
-						<audio controls src={asset.redirectedSource||asset.originalSource}>
-						</audio>
+<section>
+	{#if assets}
+		{#each assets.sort((a,b)=>a.name.localeCompare(b.name)) as asset (asset.uuid)}
+			{#if !isAssetFiltered(asset, filter)}
+				<div on:click={(e)=>onAssetClick(e, asset.uuid)} title='{asset.uuid}' class='asset' class:focused={focused===asset.uuid} class:selector={selector} class:selected={selected.includes(asset.uuid)}>
+					{#if !selector}
+						<header>{asset.name}</header>
 					{/if}
-				</article>
-			</div>
-		{/if}
-	{/each}
-{/if}
+					<article>
+						{#if asset.mimetype.startsWith('image')}
+							<img alt={asset.uuid} src={asset.redirectedSource||asset.originalSource}/>
+						{:else if asset.mimetype.startsWith('video')}
+							<video controls src={asset.redirectedSource||asset.originalSource}>
+								<track kind="captions" />
+							</video>
+						{:else if asset.mimetype.startsWith('audio')}
+							<audio controls src={asset.redirectedSource||asset.originalSource}>
+							</audio>
+						{/if}
+					</article>
+					{#if selector}
+						<header>{asset.name}</header>
+					{/if}
+				</div>
+			{/if}
+		{/each}
+	{/if}
+</section>
 
 <style>
+	section {
+		height: 100%;
+	}
 	.asset {
 		width: 10em;
 		height: 10em;
@@ -39,6 +107,21 @@
 		background: var(--tertiary);
 		color: var(--text);
 		border-radius: .25em .75em;
+		border: 2px solid var(--tertiary);
+		outline: 2px solid var(--tertiary);
+	}
+	.asset.selector {
+		width: 100%;
+		height: 3em;
+		display: grid;
+		grid-template-rows: minmax(0, 1fr);
+		grid-template-columns: 3em minmax(0, 1fr);
+	}
+	.asset.selected {
+		background: var(--tertiary-focus);
+	}
+	.asset.selected.focused {
+		border: 2px solid var(--text);
 	}
 	.asset > header {
 		padding: .25em 0;
@@ -47,6 +130,9 @@
 		text-overflow: ellipsis;
 		overflow: hidden;
 		word-break: break-word;
+		display: grid;
+		align-items: center;
+		justify-content: center;
 	}
 	.asset > article {
 		padding: .25em;
