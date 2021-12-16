a27e02dd3be4323d7f8ca6e332327c5421d32fe4
kts of kettek <kts@kettek.net>
2021-12-16 06:22:33 -0800

    Adjust settings styling; add changed check and apply

--- packages/app/src/Settings.svelte
+++ packages/app/src/Settings.svelte
@@ -4,6 +4,32 @@
 
 	import { onMount } from 'svelte'
 	import { settings } from './stores/settings';
+	import Button from './components/Button.svelte';
+	import Icon from './components/Icon.svelte';
+
+	let pendingSettings = {...$settings}
+	$: hasChanges = !equal($settings, pendingSettings)
+
+	function equal(a: any, b: any): boolean {
+		const k1 = Object.keys(a)
+		const k2 = Object.keys(b)
+
+		if (k1.length !== k2.length) {
+			return false
+		}
+
+		for (let k of k1) {
+			if (a[k] !== b[k]) {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	function applyChanges() {
+		$settings.port = pendingSettings.port
+	}
 
 	onMount(async () => {
 
@@ -14,9 +40,12 @@
 	<header> Settings </header>
 	<article>
 		<label>
-			<input type='number' bind:value={$settings.port} placeholder='port'/>
+			<input type='number' bind:value={pendingSettings.port} placeholder='port'/>
 			<span>Port</span>
 		</label>
+		<Button primary disabled={!hasChanges} on:click={applyChanges}>
+			<Icon icon="checkmark"></Icon>
+		</Button>
 	</article>
 </section>
 
@@ -27,7 +56,7 @@
 		top: calc(50% - 15em);
 		width: 30em;
 		height: 30em;
-		background: var(--bar-bg);
+		background: var(--nav-bg);
 		z-index: 9999;
 	}
 	header {
@@ -42,5 +71,13 @@
 	article {
 		color: var(--primary);
 		padding: .5em;
+	}
+	label {
+		display: grid;
+		grid-template-columns: minmax(0, 1fr) auto;
+		align-items: center;
+	}
+	label span {
+		margin-left: 0.25em;
 	}
 </style>
