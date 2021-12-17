0c63356c96d28d6f1796d939ac310a1202c25133
kts of kettek <kts@kettek.net>
2021-12-17 12:09:04 -0800

    Adjust logic; add duplicate

--- packages/app/src/stores/collections.ts
+++ packages/app/src/stores/collections.ts
@@ -1,6 +1,7 @@
 import type { Asset, Collection } from '../interfaces/Asset'
 import { localStore } from './localStore'
 import { get } from 'svelte/store'
+import { v4 } from 'uuid'
 
 export function addCollection(o: Collection) {
 	collections.update((v: any) => {
@@ -9,15 +10,24 @@ export function addCollection(o: Collection) {
 	})
 }
 
-export function removeCollection(u: string) {
-	collections.update((v: any) => {
-		v = v.filter((v: Collection)=>v.uuid===u)
-		return v
-	})
+export function removeCollection(uuid: string) {
+	let cs = get(collections)
+	cs = cs.filter((v: Collection) => v.uuid !== uuid)
+	collections.set(cs)
 }
 
 export function refreshCollections() {
 	collections.set(get(collections))
+}
+
+export function duplicateCollection(uuid: string) {
+	let cs = get(collections)
+	let c = cs.find((v: any)=>v.uuid === uuid)
+	if (!c) return
+	let c2 = JSON.parse(JSON.stringify(c)) // FIXME: Replace with clone
+	c2.uuid = v4()
+	cs.push(c2)
+	collections.set(cs)
 }
 
 export function addAsset(uuid: string, asset: Asset) {
