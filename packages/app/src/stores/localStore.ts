65442bbf123cdc98e6b082a3ce63fb287d2f600c
kts of kettek <kts@kettek.net>
2021-12-17 05:58:36 -0800

    Add localStore

---
+++ packages/app/src/stores/localStore.ts
@@ -0,0 +1,30 @@
+// localStore.ts
+import { writable } from 'svelte/store'
+import type { OverlayInterface } from '../interfaces/Overlay'
+import type { Collection } from '../interfaces/Asset'
+
+export type JsonValue = string | number | boolean | null | JsonValue[] | { [key: string]: JsonValue } | Record<string, OverlayInterface> | Collection[]
+
+export const localStore = <T extends JsonValue>(key: string, initial: T) => {
+	const toString = (value: T) => JSON.stringify(value, null, 2)
+	const toObj = JSON.parse
+
+	if (localStorage.getItem(key) === null) {
+		localStorage.setItem(key, toString(initial))
+	}
+
+	navigator.storage.persist()
+
+	const saved = toObj(window.localStorage.getItem(key))
+
+	const { subscribe, set, update } = writable<T>(saved)
+
+	return {
+		subscribe,
+		set: (value: T) => {
+			localStorage.setItem(key, toString(value))
+			return set(value)
+		},
+		update
+	}
+}
