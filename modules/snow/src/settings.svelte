0ac1afc720ccb120d6851cb9e2f115b978061ba2
kts of kettek <kts@kettek.net>
2021-12-17 10:28:00 -0800

    Adjust settings styling

--- modules/snow/src/settings.svelte
+++ modules/snow/src/settings.svelte
@@ -23,100 +23,96 @@
 </script>
 
 <main>
-	<section>
-		<fieldset>
-			<label>
-				<select bind:value={settings.sourceType}>
-					<option value='emoji'>emoji</option>
-					<option value='asset'>asset</option>
-				</select>
-				Type
-			</label>
-			{#if settings.sourceType === 'emoji'}
-				<label>
-					<input type='text' bind:value={settings.emoji}/>
-					Emoji
-				</label>
-			{:else if settings.sourceType === 'asset'}
-				<label>
-					<input type='hidden'/>
-					Asset
-					<Button title='Open file' tertiary on:click={openFileDialog}>
-						<Icon icon='open'></Icon>
-					</Button>
-				</label>
-				<div class='preview'>
-					<img alt='preview' src={assets.source(settings.reference)}/>
-				</div>
-			{/if}
-			<label>
-				<input type='number' bind:value={settings.size}/>
-				Base Size
-			</label>
-			<label>
-				<input type='number' bind:value={settings.updateRate}/>
-				Update Rate
-			</label>
-			<label>
-				<input type='number' bind:value={settings.maxAccumulator}/>
-				Max Accumulator
-			</label>
-		</fieldset>
-		<fieldset>
-			<span>Reload Required</span>
-			<label>
-				<input type='number' bind:value={settings.count}/>
-				Count
-			</label>
-			<label>
-				<input type='number' bind:value={settings.spawnX}/>
-				X Spawn
-			</label>
-			<label>
-				<input type='number' bind:value={settings.spawnY}/>
-				Y Spawn
-			</label>
-			<label>
-				<input type='number' bind:value={settings.minScale}/>
-				Min Scale
-			</label>
-			<label>
-				<input type='number' bind:value={settings.scaleRandom}/>
-				Random Scale
-			</label>
-			<label>
-				<input type='number' bind:value={settings.xRandomRate[0]}/>
-				<input type='number' bind:value={settings.xRandomRate[1]}/>
-				X Random Rate (min, max)
-			</label>
-			<label>
-				<input type='number' bind:value={settings.yRandomRate[0]}/>
-				<input type='number' bind:value={settings.yRandomRate[1]}/>
-				Y Random Rate (min, max)
-			</label>
-			<label>
-				<input type='checkbox' bind:checked={settings.rotate}/>
-				Randomize Starting Rotation
-			</label>
-			<label>
-				<input type='number' bind:value={settings.rotRandomRate[0]}/>
-				<input type='number' bind:value={settings.rotRandomRate[1]}/>
-				Rotation Random Rate (min, max)
-			</label>
-		</fieldset>
-	</section>
+	<label>
+		<select bind:value={settings.sourceType}>
+			<option value='emoji'>emoji</option>
+			<option value='asset'>asset</option>
+		</select>
+		Type
+	</label>
+	{#if settings.sourceType === 'emoji'}
+		<label>
+			<input type='text' bind:value={settings.emoji}/>
+			Emoji
+		</label>
+	{:else if settings.sourceType === 'asset'}
+		<label>
+			<input type='hidden'/>
+			Asset
+			<Button title='Open file' tertiary on:click={openFileDialog}>
+				<Icon icon='open'></Icon>
+			</Button>
+		</label>
+		<div class='preview'>
+			<img alt='preview' src={assets.source(settings.reference)}/>
+		</div>
+	{/if}
+	<label>
+		<input type='number' bind:value={settings.size}/>
+		Base Size
+	</label>
+	<label>
+		<input type='number' bind:value={settings.updateRate}/>
+		Update Rate
+	</label>
+	<label>
+		<input type='number' bind:value={settings.maxAccumulator}/>
+		Max Accumulator
+	</label>
+	<hr/>
+	<span>Reload Required</span>
+	<label>
+		<input type='number' bind:value={settings.count}/>
+		Count
+	</label>
+	<label>
+		<input type='number' bind:value={settings.spawnX}/>
+		X Spawn
+	</label>
+	<label>
+		<input type='number' bind:value={settings.spawnY}/>
+		Y Spawn
+	</label>
+	<label>
+		<input type='number' bind:value={settings.minScale}/>
+		Min Scale
+	</label>
+	<label>
+		<input type='number' bind:value={settings.scaleRandom}/>
+		Random Scale
+	</label>
+	<label>
+		<input type='number' bind:value={settings.xRandomRate[0]}/>
+		<input type='number' bind:value={settings.xRandomRate[1]}/>
+		X Random Rate (min, max)
+	</label>
+	<label>
+		<input type='number' bind:value={settings.yRandomRate[0]}/>
+		<input type='number' bind:value={settings.yRandomRate[1]}/>
+		Y Random Rate (min, max)
+	</label>
+	<label>
+		<input type='checkbox' bind:checked={settings.rotate}/>
+		Randomize Starting Rotation
+	</label>
+	<label>
+		<input type='number' bind:value={settings.rotRandomRate[0]}/>
+		<input type='number' bind:value={settings.rotRandomRate[1]}/>
+		Rotation Random Rate (min, max)
+	</label>
 </main>
 
 <style>
 	main {
 		display: grid;
-		grid-template-rows: minmax(0, 1fr);
-		height: 100%;
-	}
-	section {
+		grid-template-rows: minmax(0, 1fr) minmax(0, 1fr);
 		overflow: auto;
 	}
 	input[type=number] {
 		width: 5em;
+	}
+	hr {
+		width: 100%;
+		border-color: var(--text);
 	}
 </style>
