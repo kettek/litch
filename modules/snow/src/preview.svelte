bffd00d4f3843c67ef4645c99f6f7d52c491b048
kts of kettek <kts@kettek.net>
2021-12-17 06:39:01 -0800

    Add light previewing of snow

--- modules/snow/src/preview.svelte
+++ modules/snow/src/preview.svelte
@@ -1,10 +1,34 @@
 <script type="ts">
-	/*import type { BoxInterface } from '../../../src/interfaces/Box'
+	import type { AssetManager } from '@kettek/litch-app/src/interfaces/Asset'
+	import type { SettingsInterface } from './SettingsI'
 
-	export let settings: any = {}
-	export let box: BoxInterface*/
+	export let settings: SettingsInterface
+	export let assets: AssetManager
 </script>
 
 <div>
-	Shnow
+	<img alt='' src={assets.source(settings.reference)}/>
+	<header>Snow</header>
 </div>
+
+<style>
+	div {
+		display: grid;
+		grid-template-rows: minmax(0, 1fr) auto;
+		width: 100%;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+	img {
+		max-width: 100%;
+		max-height: 100%;
+		opacity: 0.25;
+		object-fit: contain;
+	}
+	header {
+		text-align: center;
+		background: var(--tertiary);
+		color: var(--text);
+	}
+</style>
