767ab4264473f633469bda3f1a26ac76e4993e7e
kts of kettek <kts@kettek.net>
2021-12-23 16:47:18 -0800

    Use Card

--- packages/app/src/AssetsCard.svelte
+++ packages/app/src/AssetsCard.svelte
@@ -1,17 +1,15 @@
 <script type='ts'>
 	import { _ } from 'svelte-i18n'
-	import type { Asset, Collection } from './interfaces/Asset'
+	import type { Asset } from './interfaces/Asset'
 	import { getAsset, httpReference } from './assets'
-	import { collections, refreshCollections } from './stores/collections'
-	import { createEventDispatcher, onMount } from 'svelte'
-	import { fly } from 'svelte/transition'
-	import { quintInOut } from 'svelte/easing'
-	import { publisher } from './modules'
+	import { collections } from './stores/collections'
+	import { createEventDispatcher } from 'svelte'
 	import { settings } from './stores/settings'
 	import DropList from './components/DropList.svelte'
 	import AssetsListing from './AssetsListing.svelte'
 	import Button from './components/Button.svelte'
 	import Icon from './components/Icon.svelte'
+	import Card from './components/Card.svelte'
 
 	export let multiple: boolean = false
 
@@ -44,72 +42,52 @@
 	let filter: string = ''
 </script>
 
-<main transition:fly="{{delay: 0, duration: 200, x: 500, y: 0, easing: quintInOut}}">
-	<nav>
-		<Button on:click={()=>{dispatch('close', [])}}>
-			<Icon icon="back"></Icon>
-		</Button>
-		<header>{$_('collections.selectAnAsset')}</header>
-	</nav>
-	<section>
-		<DropList secondary>
-			<svelte:fragment slot="heading">
-				{$_('collections.title')}
-			</svelte:fragment>
-			<svelte:fragment slot="content">
-				{#each $collections as collection}
-					<div on:click={()=>$settings.collectionUUID=collection.uuid}>
-						{collection.name}
-					</div>
-				{/each}
-			</svelte:fragment>
-		</DropList>
-	</section>
-	<section class='assets'>
-		<DropList tertiary>
-			<svelte:fragment slot="heading">
-				{$_('assets.title')}
-			</svelte:fragment>
-			<svelte:fragment slot="content">
-				<AssetsListing multiple={multiple} selector assets={assets} bind:focused={focusedAssetUUID} bind:selected={selectedAssetUUIDs} filter={filter}></AssetsListing>
-			</svelte:fragment>
-		</DropList>
-	</section>
-	<section class='return'>
+<Card secondary on:close>
+	<svelte:fragment slot='title'>{$_('collections.selectAnAsset')}</svelte:fragment>
+	<svelte:fragment slot='content'>
+		<main>
+			<section>
+				<DropList secondary>
+					<svelte:fragment slot="heading">
+						{$_('collections.title')}
+					</svelte:fragment>
+					<svelte:fragment slot="content">
+						{#each $collections as collection}
+							<div on:click={()=>$settings.collectionUUID=collection.uuid}>
+								{collection.name}
+							</div>
+						{/each}
+					</svelte:fragment>
+				</DropList>
+			</section>
+			<section class='assets'>
+				<DropList tertiary>
+					<svelte:fragment slot="heading">
+						{$_('assets.title')}
+					</svelte:fragment>
+					<svelte:fragment slot="content">
+						<AssetsListing multiple={multiple} selector assets={assets} bind:focused={focusedAssetUUID} bind:selected={selectedAssetUUIDs} filter={filter}></AssetsListing>
+					</svelte:fragment>
+				</DropList>
+			</section>
+		</main>
+	</svelte:fragment>
+	<svelte:fragment slot='footer'>
 		<Button tertiary on:click={returnResults}>
 			<Icon icon='checkmark'></Icon>
 		</Button>
-	</section>
-</main>
+	</svelte:fragment>
+</Card>
 
 <style>
-	main {
-		position: absolute;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		background: var(--nav-bg);
-		display: grid;
-		grid-template-rows: auto auto minmax(0, 1fr);
-	}
-	nav {
-		background: var(--primary);
-		color: var(--text);
-		display: grid;
-		grid-template-columns: auto minmax(0, 1fr);
-		align-items: stretch;
-		justify-content: stretch;
-	}
-	nav header {
-		font-weight: 600;
-		display: flex;
-		align-items: center;
-		padding-left: .5em;
-	}
 	.assets {
 		display: grid;
 		grid-template-rows: minmax(0 , 1fr);
 		margin-bottom: 2em;
+	}
+	main {
+		height: 100%;
+		display: grid;
+		grid-template-rows: auto minmax(0, 1fr);
 	}
 </style>
