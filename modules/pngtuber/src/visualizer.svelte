3d250ccf0532a9cd2a4031420945d490d18ca0e4
kts of kettek <kts@kettek.net>
2021-12-23 23:00:57 -0800

    Use Card for visualizer

--- modules/pngtuber/src/visualizer.svelte
+++ modules/pngtuber/src/visualizer.svelte
@@ -1,38 +1,29 @@
 <script lang="ts">
-import Button from "@kettek/litch-app/src/components/Button.svelte";
+	import Card from "@kettek/litch-app/src/components/Card.svelte"
 
 	export let frequencyData: number[] = []
 	export let shown: boolean
 </script>
 
-<main>
-	<nav>
-		<Button on:click={()=>{shown = false}}>back</Button>
-	</nav>
-	<div class='visualizer'>
+<Card tertiary zIndex={1} bind:open={shown}>
+	<svelte:fragment slot='title'>
+		Sensitivity
+	</svelte:fragment>
+	<div slot='content' class='visualizer'>
 		{#each frequencyData as f}
 			<section>
 				<article style="--height: {f/255*100}%;"></article>
 			</section>
 		{/each}
 	</div>
-</main>
+</Card>
 
 <style>
-	main {
-		position: absolute;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		display: grid;
-		grid-template-rows: auto minmax(0, 1fr);
-		z-index: 1;
-	}
 	.visualizer {
 		display: flex;
 		background: #000;
 		padding: .5em;
+		height: 100%;
 	}
 	section {
 		position: relative;
