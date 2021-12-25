d2364d5422a44eda3977acd17edf925c0bdf04d3
kts of kettek <kts@kettek.net>
2021-12-24 17:25:27 -0800

    Add ItemBar component

---
+++ packages/app/src/components/ItemBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let roundTop: boolean = false
+	let clazz: string = ''
+	export { clazz as class}
+</script>
+
+<nav class={`${clazz||''}`} class:roundTop >
+	<slot></slot>
+</nav>
+
+<style>
+	nav {
+		display: grid;
+		grid-auto-flow: column;
+	}
+	nav.roundTop {
+		border-radius: .75em .75em 0 0;
+	}
+</style>
