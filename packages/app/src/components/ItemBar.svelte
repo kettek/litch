dd85c982f02dcc7c2421daf06cdf3ff89fc65f4b
kts of kettek <kts@kettek.net>
2021-12-25 19:41:58 -0800

    Add flex prop to ItemBar

--- packages/app/src/components/ItemBar.svelte
+++ packages/app/src/components/ItemBar.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	export let round: string = ''
 	export let alt: boolean = false
+	export let flex: boolean = false
 	let clazz: string = ''
 	export { clazz as class}
 </script>
 
-<nav class={`${clazz||''} ${$$props.class}`} class:roundTop={round==='top'} class:roundBottom={round==='bottom'} class:alt >
+<nav class={`${clazz||''} ${$$props.class}`} class:roundTop={round==='top'} class:roundBottom={round==='bottom'} class:alt class:flex >
 	<slot></slot>
 </nav>
 
@@ -22,5 +23,10 @@
 	}
 	nav.alt {
 		background: var(--bar-bg);
+	}
+	nav.flex {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
 	}
 </style>
