4fd4db084e12114a7841cfa6af265f4847da62f7
kts of kettek <kts@kettek.net>
2021-12-25 21:03:01 -0800

    Add controls slot

--- packages/app/src/components/TabBar.svelte
+++ packages/app/src/components/TabBar.svelte
@@ -4,15 +4,31 @@
 </script>
 
 <nav class={`${clazz||''}`}>
-	<slot></slot>
+	<section>
+		<slot></slot>
+	</section>
+	<article>
+		<slot name='controls'></slot>
+	</article>
 </nav>
 
 <style>
 	nav {
+		margin: 0 .25em;
+		display: grid;
+		grid-template-columns: minmax(0, 1fr) auto;
+	}
+	section {
+		display: flex;
+		flex-direction: row;
+		/*flex-wrap: wrap;*/
+		align-items: flex-end;
+		overflow: auto;
+	}
+	article {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap;
 		align-items: flex-end;
-		margin: 0 .25em;
 	}
 </style>
