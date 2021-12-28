497370ea135fa1cc786ab036d1b11764a039c1a7
kts of kettek <kts@kettek.net>
2021-12-27 23:26:46 -0800

    Add ItemGroup component

---
+++ packages/app/src/components/ItemGroup.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+	export let label: boolean = false
+	export let count: number = 2
+	export let title: string = ''
+	console.log(count, new Array(count).fill('auto').join(' '))
+	$: repeatStr = 'minmax(0, 1fr) ' + new Array(count-1).fill('auto').join(' ')
+</script>
+
+{#if label}
+	<label title={title} class:ItemGroup={true} style='--repeat: {repeatStr};'>
+		<slot></slot>
+	</label>
+{:else}
+	<article title={title} class:ItemGroup={true} style='--repeat: {repeatStr};'>
+		<slot></slot>
+	</article>
+{/if}
+
+<style>
+	.ItemGroup {
+		display: grid;
+		grid-template-columns: var(--repeat);
+		padding: .5em;
+	}
+</style>
