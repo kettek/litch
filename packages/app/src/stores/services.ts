2969a26ae434741387cecf787628e92dfa68bff1
kts of kettek <kts@kettek.net>
2021-12-31 00:18:10 -0800

    Add add and remove service options

--- packages/app/src/stores/services.ts
+++ packages/app/src/stores/services.ts
@@ -2,4 +2,16 @@ import type { ServiceInterface } from '../interfaces/Service'
 import { localStore } from "./localStore"
 import { get } from 'svelte/store'
 
+export function addService(s: ServiceInterface) {
+	services.update((v: any) => {
+		v.push(s)
+		return v
+	})
+}
+
+export function removeService(uuid: string) {
+	services.update((v: any) => v.filter((v2: any)=>v2.uuid===uuid)
+	)
+}
+
 export const services = localStore<ServiceInterface[]>('services', [])
