2818ff316ded70a65f628f2cc2e7bac01ef070ae
kts of kettek <kts@kettek.net>
2021-12-30 21:53:54 -0800

    Handle ServiceInterface

--- packages/app/src/stores/localStore.ts
+++ packages/app/src/stores/localStore.ts
@@ -1,9 +1,10 @@
 // localStore.ts
 import { writable } from 'svelte/store'
 import type { OverlayInterface } from '../interfaces/Overlay'
+import type { ServiceInterface } from '../interfaces/Service'
 import type { Collection } from '../interfaces/Asset'
 
-export type JsonValue = string | number | boolean | null | JsonValue[] | { [key: string]: JsonValue } | Record<string, OverlayInterface> | Collection[]
+export type JsonValue = string | number | boolean | null | JsonValue[] | { [key: string]: JsonValue } | Record<string, OverlayInterface> | Collection[] | ServiceInterface[]
 
 export const localStore = <T extends JsonValue>(key: string, initial: T) => {
 	const toString = (value: T) => JSON.stringify(value, null, 2)
