cb85b3bbf8b3ba45d89feb4775704b8284b74eb5
kts of kettek <kts@kettek.net>
2023-04-14 20:18:13 -0700

    Add some locale data

--- services/twitch/src/RedeemCondition.svelte
+++ services/twitch/src/RedeemCondition.svelte
@@ -1,15 +1,12 @@
-<script type='ts'>
+<script lang='ts'>
 	import type { Format } from '@kettek/litch-app/src/interfaces/Format'
-  import type { ServiceChannel } from '@kettek/litch-app/src/interfaces/Service'
   import type { ServiceData } from './interfaces'
 	
 	interface ConditionI {
 		id: string
 	}
 
-	export let id: string
 	export let data: ServiceData
-	export let channel: ServiceChannel
 	export let format: Format
 	export let condition: ConditionI
 	export let refresh: () => void
@@ -23,7 +20,7 @@
 
 <main>
 	{#if data.redeems.length == 0}
-		<aside>No redeems found</aside>
+		<aside>{format('noRedeems')}</aside>
 	{/if}
 	<select value={condition.id} on:change={(e)=>changeCondition(e.currentTarget.value)}>
 		{#each data.redeems as redeem}
