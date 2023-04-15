cdbe091bc09ca694513c466e868e6bdb501fe52d
kts of kettek <kts@kettek.net>
2023-04-14 20:17:27 -0700

    Pass locale format to conditions

--- packages/app/src/ActionCondition.svelte
+++ packages/app/src/ActionCondition.svelte
@@ -1,6 +1,8 @@
 <script lang='ts'>
+	import { _ } from 'svelte-i18n'
   import ModuleWrapper from './ModuleWrapper.svelte'
   import type { ActionInterface } from './interfaces/Action'
+  import type { Format, FormatMessageObject } from './interfaces/Format';
   import type { ServiceChannel, ServiceInterface } from './interfaces/Service'
   import { createServiceChannel } from './services'
   import { refreshActions } from './stores/actions'
@@ -9,9 +11,14 @@
 	export let action: ActionInterface
 
 	let serviceChannel: ServiceChannel = createServiceChannel(service.uuid)
+
+	let localeFormat: Format = (messageId: string, options?: FormatMessageObject): string => {
+		return $_(`service.${service.uuid}.${messageId}`, options)
+	}
+
 </script>
 
-<ModuleWrapper this={service?.actionEvents?.conditions.find(v=>v.id===action.id)?.ConditionComponent} bind:condition={action.condition} channel={serviceChannel} data={service.data} id={action.id} refresh={refreshActions}/>
+<ModuleWrapper this={service?.actionEvents?.conditions.find(v=>v.id===action.id)?.ConditionComponent} bind:condition={action.condition} channel={serviceChannel} data={service.data} id={action.id} refresh={refreshActions} format={localeFormat}/>
 
 <style>
 </style>
