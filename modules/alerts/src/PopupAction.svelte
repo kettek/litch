aa0a8e72c889905ff30b0960ce0f07d2cac9df42
kts of kettek <kts@kettek.net>
2023-04-20 14:13:40 -0700

    Add code for handling popup trigger (but not responding)

--- modules/alerts/src/PopupAction.svelte
+++ modules/alerts/src/PopupAction.svelte
@@ -1,20 +1,17 @@
 <script lang='ts'>
 	import type { Format } from '@kettek/litch-app/src/interfaces/Format'
   import ItemGroup from '@kettek/litch-app/src/components/ItemGroup.svelte'
+  import type { PopupTriggerDataI } from './interfaces'
 	
-	interface TriggerI {
-		message: string
-	}
-
 	export let format: Format
-	export let trigger: TriggerI
+	export let data: PopupTriggerDataI
 	export let refresh: () => void
 
 </script>
 
 <main>
 	<ItemGroup label>
-		<input bind:value={trigger.message} on:change={refresh}/>
+		<input bind:value={data.message} on:change={refresh}/>
 		<svelte:fragment slot='label'>
 			{format('popup.message')}
 		</svelte:fragment>
--- modules/alerts/src/instance.svelte
+++ modules/alerts/src/instance.svelte
@@ -1,16 +1,27 @@
 <script type='ts'>
 	import { onMount } from "svelte"
 	import type { ModuleChannel } from "@kettek/litch-app/src/interfaces/ModuleInstance"
+	import type { PopupTriggerDataI } from "./interfaces"
 
 	export let channel: ModuleChannel
 
+	function triggerPopup(trigger: PopupTriggerDataI, action: Object) {
+		let msg = trigger.message
+		for (const prop in action) {
+			msg = msg.replace(new RegExp('{'+prop+'}', 'g'), action[prop])
+		}
+		console.log('output', msg)
+	}
+
 	// Republish the static server port on new live view.
 	channel.receive = async ({topic, message}) => {
-		console.log('argh', topic, message)
 		if (topic === 'refresh') {
 			//
-		} else if (topic === 'trigger') {
-			console.log('got trigger', message)
+		} else if (topic.startsWith('trigger.')) {
+			let triggerID = topic.split('.')[1]
+			if (triggerID === 'popup') {
+				triggerPopup(message.trigger, message.action)
+			}
 		}
 	}
 
