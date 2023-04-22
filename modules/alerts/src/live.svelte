d0de5165799fd619bf10370c31fe7c42382876b3
kts of kettek <kts@kettek.net>
2023-04-22 15:19:47 -0700

    Add rainbow, vibrate, wiggle, and bounce styles

--- modules/alerts/src/live.svelte
+++ modules/alerts/src/live.svelte
@@ -49,6 +49,120 @@
 		text-shadow: 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor), 0 0 2px var(--outlineColor);
 	}
 	div :global(strong) {
+		display: inline-block;
 		color: var(--focusColor);
 	}
+	div :global(strong.alerts-rainbow) {
+		text-shadow: none;
+		background: linear-gradient(to left, #6666ff, #0099ff , #00ff00, #ff3399, #6666ff, #0099ff);
+		background-clip: text;
+		-webkit-background-clip: text;
+		color: transparent;
+		animation: alertsRainbowAnimation 2s linear infinite;
+		background-size: 400% 100%;
+		padding: 0.1em;
+	}
+	div :global(strong span) {
+		display: inline-block;
+	}
+	div :global(strong.alerts-vibrate) {
+		animation: alertsVibrate 0.1s linear infinite;
+	}
+	div :global(strong.alerts-bounce) {
+		animation: alertsBounce 1s linear infinite;
+	}
+	div :global(strong.alerts-wiggle span:nth-child(4n)) {
+		animation: alertsWiggle 0.4s linear infinite;
+		animation-delay: -0.1s;
+	}
+	div :global(strong.alerts-wiggle span:nth-child(4n+1)) {
+		animation: alertsWiggle 0.4s linear infinite;
+		animation-delay: -0.2s;
+	}
+	div :global(strong.alerts-wiggle span:nth-child(4n+2)) {
+		animation: alertsWiggle 0.4s linear infinite;
+		animation-delay: -0.3s;
+	}
+	div :global(strong.alerts-wiggle span:nth-child(4n+3)) {
+		animation: alertsWiggle 0.4s linear infinite;
+		animation-delay: -0.4s;
+	}
+
+
+	@keyframes -global-alertsRainbowAnimation {
+		0% {
+			background-position: 0 0;
+		}
+
+		100% {
+			background-position: 100% 0;
+		}
+	}
+
+	@keyframes -global-alertsVibrate {
+		0% {
+			transform: translate(0, 0);
+		}
+		12.5% {
+			transform: translate(-0.03em, -0.03em);
+		}
+		25% {
+			transform: translate(0, 0);
+		}
+		37.5% {
+			transform: translate(0.03em, 0.03em);
+		}
+		50% {
+			transform: translate(0, 0);
+		}
+		62.5% {
+			transform: translate(-0.03em, 0.03em);
+		}
+		75% {
+			transform: translate(0, 0);
+		}
+		87.5% {
+			transform: translate(0.03em, -0.03em);
+		}
+		100% {
+			transform: translate(0, 0);
+		}
+	}
+
+	@keyframes -global-alertsWiggle {
+		0% {
+			transform: translateX(0) rotate(0);
+		}
+		25% {
+			transform: translateX(-0.01em) rotate(-3deg);
+		}
+		50% {
+			transform: translateX(0) rotate(0);
+		}
+		75% {
+			transform: translateX(0.01em) rotate(3deg);
+		}
+		100% {
+			transform: translateX(0) rotate(0);
+		}
+	}
+
+	@keyframes -global-alertsBounce {
+		0% {
+			transform: translateY(0.1em) scale(1.1, 0.8);
+		}
+		25% {
+			transform: translateY(-0.15em) scale(0.9, 1.0);
+		}
+		50% {
+			transform: translateY(-0.25em) scale(0.9, 1.0);
+		}
+		75% {
+			transform: translateY(-0.05em) scale(0.9, 1.0);
+		}
+		100% {
+			transform: translateY(0.1em) scale(1.1, 0.8);
+		}
+	}
+
 </style>
