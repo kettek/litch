c175411d996df2fbdc3818388318a79bac1f0bdf
kts of kettek <kts@kettek.net>
2023-04-24 01:24:19 -0700

    Pass assets manager to module trigger components

--- packages/app/src/ActionModuleTrigger.svelte
+++ packages/app/src/ActionModuleTrigger.svelte
@@ -1,8 +1,10 @@
 <script lang='ts'>
 	import { _ } from 'svelte-i18n'
   import ModuleWrapper from './ModuleWrapper.svelte'
-  import type { ActionTriggerCoreTriggerModuleI, ActionTriggerModuleI } from './interfaces/Action'
+  import type { ActionTriggerModuleI } from './interfaces/Action'
   import { refreshActions } from './stores/actions'
+	import type { AssetManager, AssetResults } from './interfaces/Asset'
+	import { createAssetManager } from './assets'
   import type { OverlayInterface } from './interfaces/Overlay'
   import { overlays } from './stores/overlays'
 	import { modules } from './stores/modules'
@@ -10,6 +12,7 @@
   import type { ModuleInterface } from './interfaces/Module'
   import type { Format, FormatMessageObject } from './interfaces/Format'
   import type { UUID } from './api'
+  import AssetsCard from './AssetsCard.svelte';
 
 	export let trigger: ActionTriggerModuleI
 	
@@ -21,13 +24,38 @@
 	$: module = overlay?.modules.find(v=>v.uuid===trigger.moduleInstanceUUID)
 	$: realModule = $modules[trigger.moduleUUID]
 
+	let showAssets = false
+	let showOptions: any = {}
+	let assetResolve: undefined | ((value: AssetResults) => void)
+	let assetReject: undefined | ((reason?: any) => void)
+
+	function closeAssets(e: any) {
+		showAssets = false
+		if (assetResolve) {
+			assetResolve(e.detail)
+		}
+		assetResolve = undefined
+		assetReject = undefined
+	}
+	// Create our assets and override the open property.
+	let assets: AssetManager = createAssetManager()
+	assets.open = (options: any): Promise<AssetResults> => new Promise((resolve, reject) => {
+		assetResolve = resolve
+		assetReject = reject
+		showOptions = options
+		showAssets = true
+	})
+
 	let localeFormat: Format = (messageId: string, options?: FormatMessageObject): string => {
 		return $_(`modules.${module?.moduleUUID}.${messageId}`, options)
 	}
 </script>
 
 {#if module && realModule?.triggerEvents}
-	<ModuleWrapper this={realModule?.triggerEvents?.actions.find(v=>v.id===trigger.triggerID)?.ActionComponent} bind:data={trigger.data} refresh={refreshActions} format={localeFormat}/>
+	<ModuleWrapper this={realModule?.triggerEvents?.actions.find(v=>v.id===trigger.triggerID)?.ActionComponent} bind:data={trigger.data} assets={assets} settings={module.settings} refresh={refreshActions} format={localeFormat}/>
+{/if}
+{#if showAssets}
+	<AssetsCard multiple={showOptions.multiple} collectionsOnly={showOptions.collectionsOnly} on:close={closeAssets}/>
 {/if}
 
 <style>
