864b596fed8e8b3e5c665fd7393454289b3a8ebe
kts of kettek <kts@kettek.net>
2023-10-04 00:06:53 -0700

    Add export config; add stub import

--- packages/app/src/Settings.svelte
+++ packages/app/src/Settings.svelte
@@ -2,6 +2,11 @@
 	import { onMount } from 'svelte'
 	import { _ } from 'svelte-i18n'
 	import { settings } from './stores/settings'
+	import { overlays } from './stores/overlays'
+	import { collections } from './stores/collections'
+	import { actions } from './stores/actions'
+	import { services } from './stores/services'
+	import ItemGroup from './components/ItemGroup.svelte'
 	import Button from './components/Button.svelte'
 	import Icon from './components/Icon.svelte'
 	import Window from './components/Window.svelte'
@@ -29,6 +34,35 @@
 	function applyChanges() {
 		$settings.port = pendingSettings.port
 	}
+	
+	function exportConfiguration() {
+		const data = JSON.stringify({
+			settings: $settings,
+			overlays: $overlays,
+			collections: $collections,
+			services: $services,
+			actions: $actions,
+		})
+		const blob = new Blob([data], {type: 'application/json'})
+		const url = URL.createObjectURL(blob)
+		const a = document.createElement('a')
+		a.href = url
+		a.download = `settings-${new Date()}.json`
+		a.click()
+		URL.revokeObjectURL(url)
+	}
+	function importConfiguration() {
+		const input = document.createElement('input')
+		input.type = 'file'
+		input.accept = 'application/json'
+		input.onchange = async (e: any) => {
+			const file = e.target.files[0]
+			const data = await file.text()
+			const config = JSON.parse(data)
+			console.log('TODO: import configuration', config)
+		}
+		input.click()
+	}
 
 	onMount(async () => {
 
@@ -38,14 +72,31 @@
 <Window primary on:close>
 	<span slot='title'>{$_('settings.title')}</span>
 	<article slot='content'>
-		<label>
+		<ItemGroup label>
 			<input type='number' bind:value={pendingSettings.port}/>
-			<span>{$_('settings.port')}</span>
-		</label>
+			<svelte:fragment slot='label'>
+				{$_('settings.port')}
+			</svelte:fragment>
+		</ItemGroup>
 		<Button primary disabled={!hasChanges} on:click={applyChanges}>
 			<Icon icon="checkmark"></Icon>
 		</Button>
-
+		<ItemGroup label>
+			<Button primary on:click={exportConfiguration}>
+				<Icon icon="open"></Icon>
+			</Button>
+			<svelte:fragment slot='label'>
+				{$_('settings.exportConfiguration')}
+			</svelte:fragment>
+		</ItemGroup>
+		<ItemGroup label>
+			<svelte:fragment slot='label'>
+				{$_('settings.importConfiguration')}
+			</svelte:fragment>
+			<Button primary on:click={importConfiguration}>
+				<Icon icon="open"></Icon>
+			</Button>
+		</ItemGroup>
 	</article>
 </Window>
 
